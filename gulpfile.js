947fdc19399b87d18c632b53698b58edcd3be7a7
LuisMarioSolano <luismariosga@gmail.com>
2023-03-15 16:52:20 -0600

    Install plumber

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 const { src, dest, watch } = require("gulp")
 const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
 
 function css (done) {
     
     src("src/scss/**/*.scss") // identificar el archivo de sass
+    .pipe(plumber()) // No detiene la ejecución del workflow cuando hay algún error
     .pipe( sass() ) // Compilarlo
     .pipe( dest("build/css") ); // Almacenarla en el disco duro
 
