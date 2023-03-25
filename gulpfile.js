4f1276a670cf80d69ea13c7091ad81dbbeb9d835
LuisMarioSolano <luismariosga@gmail.com>
2023-03-24 20:42:06 -0600

    install gulp-webp

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,42 @@
-const { src, dest, watch } = require("gulp")
+const { src, dest, watch, parallel } = require("gulp")
+
+//CSS
 const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
 
-function css (done) {
-    
+// Imagenes
+const webp = require('gulp-webp');
+
+function css(done) {
+
     src("src/scss/**/*.scss") // identificar el archivo de sass
-    .pipe(plumber()) // No detiene la ejecución del workflow cuando hay algún error
-    .pipe( sass() ) // Compilarlo
-    .pipe( dest("build/css") ); // Almacenarla en el disco duro
+        .pipe(plumber()) // No detiene la ejecución del workflow cuando hay algún error
+        .pipe(sass()) // Compilarlo
+        .pipe(dest("build/css")); // Almacenarla en el disco duro
 
     done(); //Callback que avisa a gulp cuando llegamos al final
+}
+
+function versionWebp(done) {
+
+    const opciones = {
+        quality: 50
+    };
+
+    src('src/img/**/*.{png,jpg}')
+        .pipe( webp(opciones) )
+        .pipe( dest('build/img') )
+
+
+    done();
 }
 
 function dev(done) {
     watch("src/scss/**/*.scss", css)
 
-    done ();
+    done();
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.versionWebp = versionWebp;
+exports.dev = parallel (versionWebp, dev);
