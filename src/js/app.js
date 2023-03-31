b8d638bc1fe5c690aebd0bb43940aea5b19ce6c7
LuisMarioSolano <luismariosga@gmail.com>
2023-03-30 22:40:59 -0600

    Crear botón para cerrar imagen

--- src/js/app.js
+++ src/js/app.js
@@ -23,17 +23,37 @@ function crearGaleria() {
     }
 }
 
-function mostrarImagen (id) {
+function mostrarImagen(id) {
     const imagen = document.createElement('picture');
-        imagen.innerHTML = `
+    imagen.innerHTML = `
         <source srcset="build/img/grande/${id}.avif" type="image/avif">
         <source srcset="build/img/grande/${id}.webp" type="image/webp">
         <img loading="lazy" width="200" height="300" src="build/img/thumb/${id}.jpg" alt="imagen galeria">
     `;
-    const overlay = document.createElement ('DIV');
+
+    // Crea el Overlay con la imagen
+    const overlay = document.createElement('DIV');
     overlay.appendChild(imagen);
     overlay.classList.add('overlay');
+    overlay.onclick = function () {
+        const body = document.querySelector('body');
+        body.classList.remove('fijar-body')
+        overlay.remove();
+    }
 
+    // Boton para cerrar el Modal
+    const cerrarModal = document.createElement('P');
+    cerrarModal.textContent = 'X';
+    cerrarModal.classList.add('btn-cerrar')
+    cerrarModal.onclick = function () {
+        const body = document.querySelector('body');
+        body.classList.remove('fijar-body')
+        overlay.remove();
+    }
+    overlay.appendChild(cerrarModal)
+
+    // Añadirlo al HTML
     const body = document.querySelector('body');
     body.appendChild(overlay);
+    body.classList.add('fijar-body')
 }
